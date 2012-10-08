e342af87297abc80e30fd1c07743458860f615c6
Guillermo <guillermo@guillermo-N56VM.(none)>
2012-10-08 23:50:15 +0200

    Exercise 3 done

--- HW 1-1/hw1/part3.rb
+++ HW 1-1/hw1/part3.rb
@@ -1,3 +1,26 @@
 def combine_anagrams(words)
- # YOUR CODE HERE
+
+  anagrams  = Hash.new
+
+  words.each { |word|
+    reference = word.downcase.split(//).sort.join
+
+    if anagrams.key? reference
+        anagrams[reference].push word
+    else
+        list = Array.new
+
+        list.push word
+
+        anagrams[reference] = list
+    end
+
+  }
+
+  return anagrams.values
+
 end
+
+input =['Cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']
+
+puts combine_anagrams(input).to_s
