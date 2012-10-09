9e0e496e3bcb7e24c4496d55bc72914b5849106c
Guillermo <guillermo@guillermo-N56VM.(none)>
2012-10-10 01:26:29 +0200

    Working on exercise 6

--- HW 1-1/hw1/part5.rb
+++ HW 1-1/hw1/part5.rb
@@ -3,6 +3,37 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "your code here, use %Q for multiline strings"
+    class_eval %Q"
+    	def #{attr_name}=(attr_value)
+
+    		puts 'New value: ' + attr_value.to_s
+
+    		if !defined? @#{attr_name}_history 
+    			puts 'Created....'
+    			@#{attr_name}_history = [@#{attr_name}]
+    		end
+
+    		@#{attr_name}_history.push attr_value
+    		@#{attr_name} = attr_value
+    	end
+    	"
   end
 end
+
+class Foo
+    attr_accessor_with_history :bar
+end
+
+f = Foo.new
+f.bar = 1
+puts "Value: " + f.bar.to_s
+f.bar = 2
+puts "Value: " + f.bar.to_s
+puts f.bar_history.to_s # => if your code works, should be [nil, 1, 2]
+
+f = Foo.new
+f.bar = 1
+f.bar = 2
+f = Foo.new
+f.bar = 4
+puts f.bar_history.to_s
--- HW 1-1/hw1/part6.rb
+++ HW 1-1/hw1/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,38 @@ class Numeric
       super
     end
   end
+
+  def in(currency)
+    singular_currency = currency.to_s.gsub( /s$/, '')
+    return self / @@currencies[singular_currency]
+  end
 end
 
 class String
-  # YOUR CODE HERE
+  
+  def palindrome?
+    treated_str = self.gsub(/[^\w]/,'')
+    return treated_str.downcase == treated_str.downcase.reverse
+  end  
+
 end
 
 module Enumerable
-  # YOUR CODE HERE
+  def palindrome?
+      self.to_a.each_index { |index|
+        return false unless self[index] == self[(self.length - 1) - index]
+      }
+      return true
+  end
 end
+
+
+puts 5.dollars.in(:euros).to_s
+puts 10.euros.in(:rupees).to_s
+puts 2.rupee.in(:dollar).to_s
+puts 5.rupees.in(:yen)
+
+puts "A man, a plan, a canal -- Panama".palindrome?
+puts [1,2,3,2,1].palindrome?
+puts [1,2,3,2,2].palindrome?
+#puts [1,2,3,2,2].palindrome?
